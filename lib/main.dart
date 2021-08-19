1d4d31398290a9d4b8d71afaf70e1e1b68067463
shishiny101 <elshishinya62@gmail.com>
2021-08-19 10:35:19 +0200

    Status bar fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'bmi_result_screen.dart';
 import 'bmi_screen.dart';
 
@@ -9,10 +10,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.black87));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          brightness: Brightness.dark
+        )
+      ),
       home: BmiScreen(),
     );
   }
 }
-
